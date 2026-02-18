ab8c17425486074ebbe474285cd538c766495465
Hassan <hachehri@gmail.com>
2026-02-18 23:11:31 +0100

    feat:Add cookies save and reset functionality with language switch

--- i.php
+++ i.php
@@ -1,59 +1,61 @@
 
 
-
 <?php   
    
 
-        
-    //    if(isset($_COOKIE["cookie_color"])){
-          
-    //    }
-  
+    // declare 3 variable
+    $f_name = trim($_POST["f_name"] ?? "said");
+    $bg_color = $_POST["bg_color"] ?? "#fffff";
+    $langauge = $_POST["option"] ?? "";
+    
 
-  if($_SERVER["REQUEST_METHOD"] === "POST"){
-        
-        // declare 3 variable
-        $f_name = trim($_POST["f_name"] ?? "");
-        $b_color = $_POST["bg_color"] ?? "#fffff";
-        $langauge = $_POST["option"] ?? "en";
-        
+   if(isset($_POST["rest"])){
 
+        setcookie("cookie_user", $f_name , time() - 3600);
+        setcookie("cookie_color" , $bg_color , time() -  3600);
+        setcookie("cookie_lang" , $langauge , time() - 3600);
+
+        header("location:" . $_SERVER["REQUEST_URI"]);
+        exit();
+   }
+   
+ 
+  if( $_SERVER["REQUEST_METHOD"] === "POST"){
+   
+   if(isset($_POST["save_your"])){
         // Add cookie each name variable to declare
         setcookie("cookie_user", $f_name , time() + 3600);
-        setcookie("cookie_color" , $b_color , time() + 3600);
+        setcookie("cookie_color" , $bg_color , time() + 3600);
         setcookie("cookie_lang" , $langauge , time() + 3600);
-
         
-    //   switch ($langauge){
-
-    //       case "Francais" :
-    //          $text="Bienvenue bv";
-    //         break;
-
-    //       case "English":
-    //            $text ="welcomme Mr";
-    //         break;
-           
-    //   }
-        
-        //  header("location:" . $_SERVER["REQUEST_URI"]);
-        header("location: i.php");
-        exit();
-        
+    
+        header("location:" . $_SERVER["REQUEST_URI"]);
         // header("location: i.php");
-        // exit();
-  }
+        exit();
+    
+   }
 
+}
 
-    $f_name = $_COOKIE["cookie_user"] ?? "Said";
-    $b_color = $_COOKIE["cookie_color"] ?? "#fffff";
-    $langauge = $_COOKIE["cookie_lang"] ?? "en";
+    $f_name = $_COOKIE["cookie_user"] ?? "said";
+    $bg_color = $_COOKIE["cookie_color"] ?? "#ffffff";
+    $langauge = $_COOKIE["cookie_lang"] ?? "";
+    
+    $text="";
 
+    switch ($langauge){
 
+          case "Francais" :
+             $text="Bienvenue bv";
+            break;
+
+          case "English":
+               $text ="welcomme Mr";
+            break;
+           
+      }
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -61,46 +63,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
 </head>
-<body style="background-color:  <?php  echo $_COOKIE["cookie_color"] ?>;">
 
+<body style="background-color:  <?php  echo $bg_color ?>;">
 
 
     <div>
-         
-
-        
-     <h1> <?php  $f_name ?> </h1>
+     <h1> <?php  echo $text .  $f_name ?> </h1>
 
     <form action="" method="post">
-        <input type="text" name="f_name">
-        <input type="color" name="bg_color">
+
+        <input type="text" name="f_name" value="<?php  echo $f_name ?>">
+        <br>
+        <br>
+
+        <input type="color" name="bg_color" value="<?php  echo $bg_color ?>">
+        <br>
+        <br>
 
          <select name="option" >
+             <option value="">SELECT</option>
             <option value="Francais">Français</option>
             <option value="English">English</option>
          </select>
 
+         <br>
+         <br>
+
          <input type="submit" name="save_your" value="Save">
-         <a href="#">Rest All</a>
+         <br>
+         <br>
+
+         <!-- <a href="#">Rest All</a> -->
+          <button type="submit" name="rest">Rest All</button>
          
     </form>
-
-
-
 
     </div>
     
 </body>
 </html>
-
-
-
-
-
-<!-- 
-//   echo "<style> body { background-color: " . $_COOKIE["cookie_color"]
-//               . "} <style>";
-
-// ?> -->
